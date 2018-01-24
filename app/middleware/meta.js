aef1ec392a79b8b786984c856fdcfc0e00b6cfe5
dead-horse <dead_horse@qq.com>
2018-01-24 19:39:13 +0800

    feat: add request and response event

--- app/middleware/meta.js
+++ app/middleware/meta.js
@@ -4,13 +4,19 @@
 
 'use strict';
 
-module.exports = options => {
+module.exports = (options, app) => {
+  if (options.logging) {
+    app.deprecate('please listen on request event and logging');
+  }
+
   return async function meta(ctx, next) {
     if (options.logging) {
       ctx.coreLogger.info('[meta] request started, host: %s, user-agent: %s', ctx.host, ctx.header['user-agent']);
     }
+    app.emit('request', ctx);
     await next();
     // total response time header
     ctx.set('x-readtime', Date.now() - ctx.starttime);
+    app.emit('response', ctx);
   };
 };
--- test/app/middleware/meta.test.js
+++ test/app/middleware/meta.test.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 const mm = require('egg-mock');
 const fs = require('mz/fs');
+const pedding = require('pedding');
 const utils = require('../../utils');
 
 describe('test/app/middleware/meta.test.js', () => {
@@ -21,6 +22,24 @@ describe('test/app/middleware/meta.test.js', () => {
         .get('/')
         .expect('X-Readtime', /\d+/)
         .expect(200);
+    });
+
+    it('should emit request event and response event', done => {
+      done = pedding(3, done);
+      app.once('request', ctx => {
+        assert(ctx);
+        assert(ctx.status === 404);
+        done();
+      });
+      app.once('response', ctx => {
+        assert(ctx);
+        assert(ctx.status === 200);
+        done();
+      });
+      app.httpRequest()
+        .get('/')
+        .expect('X-Readtime', /\d+/)
+        .expect(200, done);
     });
   });
 
